d7ce64b3f52fd498b8fb75deb39b54a671c2bf20
bluenote <janitachalam@gmail.com>
2023-02-17 12:00:08 -0500

    start message parsing

--- config/config.py
+++ config/config.py
@@ -4,14 +4,12 @@ class Config:
 
     # Zulip stream to listen on
     ZULIP_STREAM = "general"
-
+    # Cosmos chain ID
     CHAIN_ID = "juno-1"
-
-    LCD_URL = ""
-
-    AGENT_MNEMONIC = ""
-
-    CONTRACT_ADDRESS = ""
+    # Cosmos LCD URL
+    LCD_URL = "https://lcd-juno.keplr.app"
+    # Contract address to send messages to
+    CONTRACT_ADDRESS = "juno1jeq3xqkn9kypghjeqjnhsdlnjsltajm8r3s80tem3juxsjlfan3s2kzmsd"
 
     # Other configuration settings
     # ...
